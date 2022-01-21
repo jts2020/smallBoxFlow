2e421ff00514d3465e7797299f79b94b8a484d62
MainTank <maintank2020@126.com>
2022-01-21 09:47:45 +0800

    up workflow

--- src/test/java/com/jts/small/flow/test/activiti/ActivitiTest.java
+++ src/test/java/com/jts/small/flow/test/activiti/ActivitiTest.java
@@ -2,19 +2,56 @@ package com.jts.small.flow.test.activiti;
 
 import com.jts.small.flow.test.SmallFlowTest;
 import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Test;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class ActivitiTest extends SmallFlowTest {
 
     @Resource
     private RuntimeService runtimeService = null;
 
+    @Resource
+    private TaskService taskService = null;
+
     @Test
     public void test(){
         System.out.println("=========");
+        String instanceKey = "Process_1";
+        Map<String,Object> map = new HashMap<>();
+        map.put("userId","0001");
+        ProcessInstance instance = runtimeService.startProcessInstanceByKey(instanceKey);
+
+        System.out.println(instance.getId());
+        System.out.println(instance.getProcessDefinitionId());
     }
 
+    @Test
+    public void test1(){
+        System.out.println("=========");
+        String instanceKey = "Process_1";
+        Map<String,Object> map = new HashMap<>();
+        map.put("userId","0001");
+        ProcessInstance instance = runtimeService.startProcessInstanceByKey(instanceKey,map);
+        System.out.println(instance.getId());
+        System.out.println(instance.getProcessDefinitionId());
+
+        Task task = taskService.createTaskQuery().processInstanceId(instance.getId()).singleResult();
+        if(Objects.nonNull(task)){
+            Map<String,Object> map2 = new HashMap<>();
+            map2.put("userId","0002");
+            taskService.complete(task.getId(),map2);
+            System.out.println("Task:"+task.getId());
+        }else{
+            System.out.println("No found id"+instance.getId());
+        }
+
+    }
 
 }

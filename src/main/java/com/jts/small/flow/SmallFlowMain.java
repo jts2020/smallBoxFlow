0fd9318b0b7857603a82b08b5ca23abbb82f8cc8
MainTank <maintank2020@126.com>
2022-09-27 15:42:34 +0800

    up workflow

--- src/main/java/com/jts/small/flow/SmallFlowMain.java
+++ src/main/java/com/jts/small/flow/SmallFlowMain.java
@@ -1,9 +1,7 @@
 package com.jts.small.flow;
 
-import org.camunda.bpm.application.PostDeploy;
-import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.event.EventListener;
@@ -14,16 +12,13 @@ import org.springframework.context.event.EventListener;
 @EnableProcessApplication
 public class SmallFlowMain {
 
-    @Autowired
-    private RuntimeService runtimeService;
-
     public static void main(String[] args) {
         SpringApplication.run(SmallFlowMain.class, args);
     }
 
     @EventListener
-    private void processPostDeploy(PostDeploy postDeploy){
-        System.out.println(postDeploy);
+    private void processPostDeploy(PostDeployEvent event){
+        System.out.println(event.getProcessEngine().getName());
         //runtimeService.startProcessInstanceByKey("Process_1");
     }
 }
